e9d4fc460db809c3c60513b4b122957a3f103424
百里(barry) <kooksee@163.com>
2022-07-20 23:31:00 +0800

    fix: update

--- src/pages/2-offer-service/offer-page.js
+++ src/pages/2-offer-service/offer-page.js
@@ -1,87 +1,69 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 
-import { Col, Row, Typography, notification, message } from "antd";
+import {Col, message, notification, Row, Typography} from "antd";
 import OfferServiceCard from "./offer-card";
-import { StateContext, CatalogContext } from "../../constant";
+import {CatalogContext, StateContext} from "../../constant";
 
 import SelectedCatalogItemCard from "../../components/selected-catalog-item-card";
-
-import { GetOfferStreamRequest } from "../../grpc/offerpb/offer";
-import { OfferClient } from "../../grpc/offerpb/offer.client";
+import {OfferClient} from "../../grpc/offerpb/offer.client";
 
 import {GrpcWebFetchTransport} from "@protobuf-ts/grpcweb-transport";
 
 const transport = new GrpcWebFetchTransport({
-  baseUrl: "http://localhost:8000"
+    baseUrl: "http://localhost:8000"
 });
 
 const offer_client = new OfferClient(transport);
 
 function OfferService() {
-  const [selectedCatalogItem, _] = useContext(CatalogContext);
+    const [selectedCatalogItem, _] = useContext(CatalogContext);
 
-  const [offers, setOffers] = useState([]);
+    const [offers, setOffers] = useState([]);
 
-  useEffect(() => {
-    let data = [];
+    useEffect(async () => {
+        let data = [];
 
-    let req = new GetOfferStreamRequest();
-    req.setFlowId(globalState.flow_id);
-    req.setCatalogId(selectedCatalogItem.id);
+        try {
+            let stream = offer_client.getOfferStream({
+                flowId: globalState.flow_id, catalogId: selectedCatalogItem.id
+            });
 
-    let stream = offer_client.getOfferStream(req, {});
+            for await (let response of stream.responses) {
+                data.push(response);
+                setOffers([...data]);
+                message.info(`One New Offer, Total: ${data.length}`, 0.4);
+            }
 
-    stream.on("data", (response) => {
-      data.push(response.toObject());
-      setOffers([...data]);
-      message.info(`One New Offer, Total: ${data.length}`, 0.4);
-    });
+            notification.success({
+                message: "Offer Service", description: "GetOfferStream completed", duration: 2,
+            });
+        } catch (e) {
+            notification.error({
+                message: "Offer Service", description: "Error at GetOfferStream", duration: 2,
+            })
+        }
+    }, []);
 
-    stream.on("end", async () => {
-      notification.success({
-        message: "Offer Service",
-        description: "GetOfferStream completed",
-        duration: 2,
-      });
-    });
+    const [globalState, setGlobalState] = useContext(StateContext);
 
-    stream.on("error", () =>
-      notification.error({
-        message: "Offer Service",
-        description: "Error at GetOfferStream",
-        duration: 2,
-      })
-    );
-
-    return () => {
-      stream.cancel();
-    };
-  }, []);
-
-  const [globalState, setGlobalState] = useContext(StateContext);
-
-  return (
-    <>
-      <Row gutter={[16, 16]}>
-        <Col xs={24} xl={8}>
-          <Typography.Title level={4}>Selected Catalog Item</Typography.Title>
-          <SelectedCatalogItemCard />
-        </Col>
-        <Col xs={24} xl={16}>
-          <Typography.Title level={4}>Live Offers</Typography.Title>
-          <div style={{ margin: "1.2em" }}>
-            <Row gutter={[8, 8]}>
-              {offers.map((i) => (
-                <Col xs={24} xl={8} key={i.id}>
-                  <OfferServiceCard item={i} />
-                </Col>
-              ))}
-            </Row>
-          </div>
-        </Col>
-      </Row>
-    </>
-  );
+    return (<>
+        <Row gutter={[16, 16]}>
+            <Col xs={24} xl={8}>
+                <Typography.Title level={4}>Selected Catalog Item</Typography.Title>
+                <SelectedCatalogItemCard/>
+            </Col>
+            <Col xs={24} xl={16}>
+                <Typography.Title level={4}>Live Offers</Typography.Title>
+                <div style={{margin: "1.2em"}}>
+                    <Row gutter={[8, 8]}>
+                        {offers.map((i) => (<Col xs={24} xl={8} key={i.id}>
+                            <OfferServiceCard item={i}/>
+                        </Col>))}
+                    </Row>
+                </div>
+            </Col>
+        </Row>
+    </>);
 }
 
 export default OfferService;
